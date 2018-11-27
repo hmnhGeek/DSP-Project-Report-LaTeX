a436eedbbed1c1f2ffeff109eb39f73c9a3da182
hmnhGeek <hmnhsharma97@gmail.com>
2018-11-27 19:58:03 +0530

    modified GUI, changed backgound

--- Project Codes/PVD/stego-maker-gui.py
+++ Project Codes/PVD/stego-maker-gui.py
@@ -9,7 +9,14 @@ import pvd.bit_planes.bit_planes as pvd_bits
 import numpy as np
 import threading
 
+"""
+
+    AUTHOR: Himanshu Sharma
+
+"""
+
 class Page(Frame):
+    # Page class is a standard class to define frames in the Tk() window.
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
 
@@ -17,34 +24,39 @@ class Page(Frame):
         self.lift()
 
 class BitPlanePage(Page):
+    # This Page shows the bit planes. Inherits Page class.
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
+        self.configure(background='black')
 
         self.header = Label(self, text="Bit-Plane Generator", font=("consolas", 30), bg = "#00FF87", fg = "#FF003A").pack(fill=BOTH)
 
-        self.image = Label(self, text="", font=('courier', 20), fg="#83FF00")
+        self.image = Label(self, text="", font=('courier', 20), fg="#83FF00", bg='black')
         self.image.pack()
         
-        self.title_header = Label(self, text="Enter plane number (1-8)", font=("consolas", 15), fg = "#00FF87").pack()
-        self.plane = ttk.Entry(self)
-        self.plane.pack()
+        self.title_header = Label(self, text="Enter plane number (1-8)", font=("consolas", 15), fg = "#00FF87", bg='black').pack()
+        self.plane = ttk.Entry(self, width=50)
+        self.plane.pack(pady=10)
 
-        self.choose = Button(self, text="Browse", command=self.browse, width=10)
+        self.choose = Button(self, text="Browse", command=self.browse, width=30)
         self.choose.pack()
 
-        self.gen_bit_plane_button = Button(self, text="Generate", command=self.generate_bit_plane, width=10, fg="black", bg="red")
+        self.gen_bit_plane_button = Button(self, text="Generate", command=self.generate_bit_plane, width=30, fg="black", bg="red")
         self.gen_bit_plane_button.pack()
         self.gen_bit_plane_button.config(state="disabled")
 
-        self.goback = Button(self, text="Go Back", command=lambda: main.hp.show(), width=10)
+        self.goback = Button(self, text="Go Back", command=lambda: main.hp.show(), width=30)
         self.goback.pack()
 
     def browse(self):
-        f = tfd.askopenfile()
-        self.image['text'] = f.name
+        try:
+            f = tfd.askopenfile()
+            self.image['text'] = f.name
 
-        if self.image['text'] != "":
-            self.gen_bit_plane_button.config(state="normal")
+            if self.image['text'] != "":
+                self.gen_bit_plane_button.config(state="normal")
+        except AttributeError:
+            pass
 
     def generate_bit_plane(self):
         try:
@@ -58,18 +70,21 @@ class BitPlanePage(Page):
             tkMessageBox.showwarning("Alert", "Something is wrong.")
 
 class StegoGeneratorPage(Page):
+    # This page is for Stego generation.
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
 
+        self.configure(background='black')
+
         self.header = Label(self, text="Stego-Generator", font=("consolas", 30), bg = "#00FF87", fg = "#FF003A").pack(fill=BOTH)
         
-        self.file_chosen = Label(self, text="", font=('courier', 20), fg="#83FF00")
+        self.file_chosen = Label(self, text="", font=('courier', 20), fg="#83FF00", bg='black')
         self.file_chosen.pack(pady=5)
 
-        self.message_chosen = Label(self, text="", font=('courier', 20), fg="#FF3A00")
+        self.message_chosen = Label(self, text="", font=('courier', 20), fg="#FF3A00", bg='black')
         self.message_chosen.pack(pady=5)
 
-        self.block_size = ttk.Entry(self)
+        self.block_size = ttk.Entry(self, width=50)
         self.block_size.pack(pady=5)
         self.block_size.insert(END, "Block size goes here...")
 
@@ -87,11 +102,11 @@ class StegoGeneratorPage(Page):
         self.goback_button = Button(self, text="Go Back", command=self.goback, width=25)
         self.goback_button.pack()
 
-        Status_Static = Label(self, text="Status", font=('courier', 15), fg="#FF0017").pack()
-        self.status = Label(self, text="Nothing happening right now...", fg="#00FF87", font=('courier', 12))
+        Status_Static = Label(self, text="Status", font=('courier', 15), fg="#FF0017", bg='black').pack()
+        self.status = Label(self, text="Nothing happening right now...", fg="#00FF87", font=('courier', 12), bg='black')
         self.status.pack()
 
-        self.selected_image = Label(self)
+        self.selected_image = Label(self, bg='black')
         self.selected_image.pack(fill=BOTH, expand=YES)
 
     def goback(self):
@@ -106,23 +121,29 @@ class StegoGeneratorPage(Page):
         return image
 
     def select_cover(self):
-        f = tfd.askopenfile()
-        self.file_chosen['text'] = f.name
+        try:
+            f = tfd.askopenfile()
+            self.file_chosen['text'] = f.name
 
-        img = Image.open(f.name)
-        self.img = self.resize_photo(img, root.winfo_width()-10)
-        self.img = ImageTk.PhotoImage(self.img)
-        self.selected_image.configure(image = self.img)
-        
-        if self.message_chosen['text'] != "":
-            self.embedd_button.config(state="normal")
+            img = Image.open(f.name)
+            self.img = self.resize_photo(img, root.winfo_width()-10)
+            self.img = ImageTk.PhotoImage(self.img)
+            self.selected_image.configure(image = self.img)
+            
+            if self.message_chosen['text'] != "":
+                self.embedd_button.config(state="normal")
+        except AttributeError:
+            pass
 
     def select_message(self):
-        f = tfd.askopenfile()
-        self.message_chosen['text'] = f.name
+        try:
+            f = tfd.askopenfile()
+            self.message_chosen['text'] = f.name
 
-        if self.file_chosen['text'] != "":
-            self.embedd_button.config(state="normal")
+            if self.file_chosen['text'] != "":
+                self.embedd_button.config(state="normal")
+        except AttributeError:
+            pass
 
     def hide(self):
         f = open(self.message_chosen['text'], 'r')
@@ -138,15 +159,17 @@ class StegoGeneratorPage(Page):
             tkMessageBox.showwarning("Alert", "Something is wrong!!")
 
 class StegoDecoderPage(Page):
+    # this page is for decoding the stego image.
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
+        self.configure(background='black')
 
         self.header = Label(self, text="Stego-Exposer", font=("consolas", 30), bg = "#00FF87", fg = "#FF003A").pack(fill=BOTH)
 
-        self.file_chosen = Label(self, text="",  font=('courier', 20), fg="#83FF00")
+        self.file_chosen = Label(self, text="",  font=('courier', 20), fg="#83FF00", bg='black')
         self.file_chosen.pack()
 
-        self.key_chosen = Label(self, text="", font=('courier', 20), fg="#FF3A00")
+        self.key_chosen = Label(self, text="", font=('courier', 20), fg="#FF3A00", bg='black')
         self.key_chosen.pack()
 
         self.choose_file = Button(self, text="Choose Stego-Image", command=self.choose_stego, width=25)
@@ -171,29 +194,36 @@ class StegoDecoderPage(Page):
         self.clear = Button(self, text="Clear", command=lambda: self.area.delete('1.0', END), width=25)
         self.clear.pack()
 
-        self.heading = Label(self, text="Decoded text will be displayed here.", font=('courier', 15), fg="#00FF87").pack()
+        self.heading = Label(self, text="Decoded text will be displayed here.", font=('courier', 15), fg="#00FF87"
+                             , bg='black').pack()
         scrollbar = Scrollbar(self)
         scrollbar.pack(side = RIGHT, fill = Y)
-        self.area = Text(self, yscrollcommand = scrollbar.set)
+        self.area = Text(self, yscrollcommand = scrollbar.set, bg="#837E7D", fg='black')
         self.area.pack(expand=True, fill='both')
         scrollbar.config(command = self.area.yview)
 
     def choose_stego(self):
-        f = tfd.askopenfile()
-        self.file_chosen['text'] = f.name
-        self.img = Image.open(self.file_chosen['text'])
+        try:
+            f = tfd.askopenfile()
+            self.file_chosen['text'] = f.name
+            self.img = Image.open(self.file_chosen['text'])
 
-        self.show_image.config(state="normal")
-        if self.key_chosen['text'] != "":
-            self.decode_button.config(state="normal")
+            self.show_image.config(state="normal")
+            if self.key_chosen['text'] != "":
+                self.decode_button.config(state="normal")
+        except AttributeError:
+            pass
 
     def choose_key_file(self):
-        f = tfd.askopenfile()
-        self.key_chosen['text'] = f.name
+        try:
+            f = tfd.askopenfile()
+            self.key_chosen['text'] = f.name
 
-        if self.file_chosen['text'] != "":
-            self.decode_button.config(state="normal")
-            self.show_image.config(state="normal")
+            if self.file_chosen['text'] != "":
+                self.decode_button.config(state="normal")
+                self.show_image.config(state="normal")
+        except AttributeError:
+            pass
 
     def display(self):
         plt.imshow(self.img)
@@ -203,27 +233,60 @@ class StegoDecoderPage(Page):
         self.message = pvd.pvd.decode(self.file_chosen['text'], self.key_chosen['text'])
         self.area.delete('1.0', END)
         self.area.insert(END, self.message)
-        
-class HomePage(Page):
+
+class DevPage(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
+        self.configure(background='black')
 
-        self.heading = Label(self, text = "Steganographer", font=('consolas', 30), fg="#00FF6C").pack(pady=10)
+        scrollbar = Scrollbar(self)
+        scrollbar.pack(side = RIGHT, fill = Y)
+        self.area = Text(self, yscrollcommand = scrollbar.set, bg="black", fg='white', bd=0)
+        self.area.pack(expand=True, fill='both')
+        scrollbar.config(command = self.area.yview)
+        self.area.tag_configure("center", justify='center')
+        
+        abtText = open('about.txt', 'r')
+        text = abtText.read()
+        abtText.close()
+        
+        self.area.insert(END, text)
+        self.area.configure(state="disabled")
+        self.area.tag_add("center", 1.0, "end")
 
+        self.goback_button = Button(self, text="Go Back", command=lambda: main.hp.show(), width=25)
+        self.goback_button.pack()
+        
+class HomePage(Page):
+    # Homepage.
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        self.configure(background='black')
+
+        self.heading = Label(self, text = "Image Steganographer", bg = 'black', font=('consolas', 30), fg="#00FF6C").pack(pady=10)
         self.stego_generator_button = Button(self, text="Stego-Generator", command=lambda: main.sgp.show(), width=19
                                              , bg="white", fg="red", height=5)
         self.stego_generator_button.pack(pady=10)
+        self.stego_generator_button.place(x=80, y=90)
 
         self.stego_decoder_button = Button(self, text="Stego-Exposer", command=lambda: main.sdp.show(), width=19
                                            , bg="white", fg="red", height=5)
         self.stego_decoder_button.pack(pady=10)
+        self.stego_decoder_button.place(x=420, y=90)
 
         self.stego_decoder_button = Button(self, text="Bit Plane Generator", command=lambda: main.bpp.show(), width=19
                                            , bg="white", fg="red", height=5)
         self.stego_decoder_button.pack(pady=10)
+        self.stego_decoder_button.place(x=80, y=230)
 
-        self.icon_label = Label(self)
+        self.devbutton = Button(self, text="About this Software", command=lambda: main.dev.show(), width=19
+                                           , bg="white", fg="red", height=5)
+        self.devbutton.pack(pady=10)
+        self.devbutton.place(x=420, y=230)
+        
+        self.icon_label = Label(self, bg='black')
         self.icon_label.pack()
+        self.icon_label.place(x=245, y=400)
 
         icon = Image.open('logo.png')
         icon = ImageTk.PhotoImage(icon)
@@ -239,6 +302,7 @@ class MainView(Frame):
         self.sgp = StegoGeneratorPage()
         self.sdp = StegoDecoderPage()
         self.bpp = BitPlanePage()
+        self.dev = DevPage()
 
         self.container = Frame(self)
         self.container.pack(side='top', fill='both', expand=True)
@@ -247,12 +311,13 @@ class MainView(Frame):
         self.sgp.place(in_=self.container, x=0, y=0, relwidth=1, relheight=1)
         self.sdp.place(in_=self.container, x=0, y=0, relwidth=1, relheight=1)
         self.bpp.place(in_=self.container, x=0, y=0, relwidth=1, relheight=1)
+        self.dev.place(in_=self.container, x=0, y=0, relwidth=1, relheight=1)
         
         self.hp.show()
 
 if __name__ == '__main__':
     root = Tk()
-
+    root.configure(background='black')
     main = MainView(root)
     main.pack(side = 'top', fill = 'both', expand = True)
 
@@ -262,5 +327,4 @@ if __name__ == '__main__':
     root.title('Steganographer')
 
     root.mainloop()
-        
         
